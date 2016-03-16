af463e0b89ac8cd91b703db704bbe5d5f0c8c581
Gen Lu <gen.lu@microsoft.com>
2016-03-16 14:57:38 -0700

    Add stub files for SkippedTestMustHaveLinkToCorrespondingWorkItem analyzer

---
+++ src/Roslyn.Diagnostics.Analyzers/Core/SkippedTestMustHaveLinkToCorrespondingWorkItem.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using Analyzer.Utilities;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Roslyn.Diagnostics.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public class SkippedTestMustHaveLinkToCorrespondingWorkItem : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Roslyn.Diagnostics.Analyzers/UnitTests/SkippedTestMustHaveLinkToCorrespondingWorkItemTests.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.UnitTests;
+using Roslyn.Diagnostics.Test.Utilities;
+using Xunit;
+
+namespace Roslyn.Diagnostics.Analyzers.UnitTests
+{
+    class SkippedTestMustHaveLinkToCorrespondingWorkItemTests
+    {
+    }
+}
